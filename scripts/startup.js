e251a6c9487bee411aaf1c04e2b348d7c666f93c
みよ <135030944+yuri-kiss@users.noreply.github.com>
2024-09-13 10:01:25 -0400

    E

--- scripts/startup.js
+++ scripts/startup.js
@@ -1,168 +1,202 @@
-// Storage (Public)
-let csStorage = localStorage.getItem("CS-Storage");
-if (csStorage === null) csStorage = { dark: true };
-else csStorage = (() => {
-  try {
-    return JSON.parse(csStorage);
-  } catch {
-    console.warn("Storage Error, couldnt parse JSON");
-    return { dark: true };
+window.GUI_Imports = new (function() {
+  this.URLParams = new URLSearchParams(window.location.search);
+  this.EventEmitter = class EventEmitter {
+    _eventHandlers = {};
+    isValidType(type) {
+      return typeof type === "string";
+    }
+    isValidHandler(handler) {
+      return typeof handler === "function";
+    }
+    on(type, handler) {
+      if (!type || !handler) return false;
+      if (!this.isValidType(type)) return false;
+      if (!this.isValidHandler(handler)) return false;
+      let handlers = this._eventHandlers[type];
+      if (!handlers) handlers = this._eventHandlers[type] = [];
+      if (handlers.indexOf(handler) >= 0) return false;
+      handler._once = false;
+      handlers.push(handler);
+      return true;
+    }
+    once(type, handler) {
+      if (!type || !handler) return false;
+      if (!this.isValidType(type)) return false;
+      if (!this.isValidHandler(handler)) return false;
+      const ret = this.on(type, handler);
+      if (ret) handler._once = true;
+      return ret;
+    }
+    off(type, handler) {
+      if (!type) return this.offAll();
+      if (!handler) {
+        this._eventHandlers[type] = [];
+        return;
+      }
+      if (!this.isValidType(type)) return;
+      if (!this.isValidHandler(handler)) return;
+      const handlers = this._eventHandlers[type];
+      if (!handlers || !handlers.length) return;
+      for (let i = 0; i < handlers.length; i++) {
+        const fn = handlers[i];
+        if (fn === handler) {
+          handlers.splice(i, 1);
+          break;
+        }
+      }
+    }
+    offAll() {
+      this._eventHandlers = {};
+    }
+    emit(type, data) {
+      if (!type || !this.isValidType(type)) return;
+      const handlers = this._eventHandlers[type];
+      if (!handlers || !handlers.length) return;
+      const event = this.createEvent(type, data);
+      for (const handler of handlers) {
+        if (!this.isValidHandler(handler)) continue;
+        if (handler._once) event.once = true;
+        handler(event);
+        if (event.once) this.off(type, handler);
+      }
+    }
+    has(type, handler) {
+      if (!type || !this.isValidType(type)) return false;
+      const handlers = this._eventHandlers[type];
+      if (!handlers || !handlers.length) return false;
+      if (!handler || !this.isValidHandler(handler)) return true;
+      return handlers.indexOf(handler) >= 0;
+    }
+    getHandlers(type) {
+      if (!type || !this.isValidType(type)) return [];
+      return this._eventHandlers[type] || [];
+    }
+    createEvent(type, data, once = false) {
+      const event = { type, data, timestamp: Date.now(), once };
+      return event;
+    }
   }
 })();
-
-/*
-  save csStorage to localStorage (publicly accessible)
-*/
-function saveStorage() {
-  localStorage.setItem("CS-Storage", JSON.stringify(csStorage));
-}
-
-// Event Emitter (Public)
-const EventEmitter = (() => {
-  const events = {};
-  return {
-    emit(eName, ...args) {
-      if (!events[eName]) events[eName] = [];
-      events[eName].forEach(func => func(...args));
-    },
-    on(eName, func) {
-      if (!events[eName]) events[eName] = [];
-      events[eName].push(func);
-    },
-    once(eName, func) {
-      const wrapper = (...args) => {
-        func(...args);
-        this.off(eName, wrapper);
-      };
-      this.on(eName, wrapper);
-    },
-    off(eName, func) {
-      if (!events[eName]) return;
-      events[eName] = events[eName].filter(storedFunc => storedFunc !== func);
-    }
-  };
-})();
-
-// Pre-done Events
-EventEmitter.on("mode-switch", (val, btn, childs) => {
-  btn.setAttribute("darkMode", val);
-  childs[0].src = val === false ? "/site-real/assets/dark.svg" : "/site-real/assets/light.svg";
-  childs[1].textContent = val === false ? "Dark Mode" : "Light Mode";
-  document.body.style.background = val === false ? "#fff" : "#141414";
-});
-
-EventEmitter.on("tab-click", (name) => {
-  const navBtns = document.querySelectorAll(`div[class="nav-btn"]`);
-  for (let i = 1; i < navBtns.length; i++) {
-    const btn = navBtns[i];
-    if (btn.id !== name) btn.style.display = "";
-    else if (btn.id !== "home") {
-      createHomeBtn(btn);
-      btn.style.display = "none";
-    }
-  }
-});
-
-// Tab Loader (Public variables)
-const siteParams = new URLSearchParams(window.location.search);
-let thisPage = siteParams.get("page");
-if (!thisPage) thisPage = "home";
-setTab(thisPage);
-
-/*
-  remove imported tab and its script
-*/
-function resetTab() {
-  const innerBody = document.querySelector(`div[class="content-body"]`);
-  if (innerBody) innerBody.remove();
-  const tabScripts = document.querySelectorAll(`script[id="page-loader"]`);
-  for (let i = 0; i < tabScripts.length; i++) tabScripts[i].remove();
-}
-
-/*
-  set the tab by name and import its scripts
-  defaults to the home page
-*/
-function setTab(name) {
-  resetTab();
-  thisPage = (() => {
-    // in case we add more pages
-    switch (name) {
-      case "about": return "about";
-      case "submit": return "submit";
-      case "exports": return "exports";
-      case "tos": return "tos";
-      case "team": return "team";
-      default: return "home";
-    }
+window.GUI = new (function() {
+  this.globalEvents = new GUI_Imports.EventEmitter();
+  this.csStorage = new (function() {
+    const key = 'CS-Storage';
+    const defaultConfig = {
+      dark: true,
+    };
+    this.data = null;
+    this.refresh = function(update) {
+      if (update) {
+        this.data = update;
+        localStorage.setItem(key, JSON.stringify(this.data));
+      }
+      try {
+        return JSON.parse(localStorage.getItem(key));
+      } catch {
+        console.warn('Storage Error, couldnt parse JSON');
+        this.refresh(this.defaultConfig);
+      }
+    };
+    this.refresh();
   })();
-  siteParams.set("page", thisPage);
-  const newUrl = `${window.location.pathname}?${siteParams.toString()}`;
-  window.history.replaceState({}, "", newUrl);
-
-  const script = document.createElement("script");
-  script.id = "page-loader";
-  script.src = `/site-real/scripts/${thisPage}-page.js`;
-  document.body.appendChild(script);
-  EventEmitter.emit("tab-click", thisPage);
-}
-
-// Attach Button Listeners
-function attachBtnListeners() {
-  // logo button (home)
-  const logoBtn = document.querySelector(`nav[class="nav-bar"] div[class="logo"]`)
-  logoBtn.addEventListener("click", () => {
-    const anyHomeBtn = document.querySelector(`div[class="nav-btn"][id="home"]`);
-    if (anyHomeBtn !== null) anyHomeBtn.remove();
-    setTab("home");
-    EventEmitter.emit("tab-click", "home");
+  
+  this.globalEvents.on('theme-switch', (val, btn, children) => {
+    btn.setAttribute('darkMode', val);
+    children[0].src = val === false ? './assets/dark.svg' : './assets/light.svg';
+    children[1].textContent = val === false ? 'Dark Mode' : 'Light Mode';
+    document.body.style.background = val === false ? '#ffffff' : '#141414';
   });
-
-  // light/dark mode
-  const modeBtn = document.querySelector(`div[id="mode-switch"]`);
-  const childs = modeBtn.children;
-  modeBtn.setAttribute("darkMode", csStorage.dark);
-  modeBtn.addEventListener("click", () => {
-    const val = !(modeBtn.getAttribute("darkMode") === "true");
-    csStorage.dark = val;
-    EventEmitter.emit("mode-switch", val, modeBtn, childs);
-    saveStorage();
+  this.globalEvents.on('tab-switch', (name) => {
+    for (const btn of this.nav.buttons) {
+      if (btn.dataset.name !== name) btn.style.display = '', continue;
+      if (btn.dataset.name !== 'home') {
+        this.nav.spawn('home', btn);
+        btn.style.display = 'none';
+      }
+    }
   });
-  if (csStorage.dark === false) EventEmitter.emit("mode-switch", csStorage.dark, modeBtn, childs);
+  
+  this.tab = new (function(GUI, GUI_Imports) {
+    this.current = GUI_Imports.URLParams.get('page') || 'home';
+    this.contentBody = new (function() {
+      this.scripts = [];
+      this.node = document.querySelector('div.content-body') || null;
+      this.set = function(node) {
+        if (!node) return this.node = document.querySelector('div.content-body');
+        this.node = node;
+      };
+      this.removeScripts = function() {
+        let script;
+        while(script && (script = this.scripts.shift())) script.remove();
+      };
+    })();
+    this.reset = function() {
+      this.contentBody.remove();
+      this.contentBody.removeScripts();
+    };
+    this.set = function(name) {
+      this.reset();
+      GUI_Imports.URLParams.set('page', name);
+      this.current = name;
+      const newUrl = `${window.location.pathname}?${GUI_Imports.URLParams.toString()}`;
+      window.history.replaceState({}, '', newUrl);
+      const script = document.createElement('script');
+      script.id = 'page-loader';
+      script.src = `./scripts/${name}-page.js`;
+      this.scripts.push(script);
+      document.body.appendChild(script);
+      GUI.globalEvents.emit('tab-switch', name);
+    };
+  })(this, GUI_Imports);
+  this.tab.set(this.tab.current);
+  
+  this.nav = new (function(GUI) {
+    const { tab, globalEvents, csStorage } = GUI;
+    this.buttons = [];
+    this.node = document.querySelector('nav.nav-bar');
+    this.logo = document.querySelector('nav.nav-bar div.logo');
+    this.theme = document.querySelector('div#theme-switch');
+    this.home = document.querySelector(`div.nav-btn[data-name="home"]`)
+    this.attachListeners = function() {
+      this.logo.addEventListener('click', () => {
+        if (this.home) {
+          this.home.remove();
+          this.home = null;
+        };
+        tab.set('home');
+        globalEvents.emit('tab-switch', 'home');
+      });
+      this.mode.dataset.dark = String(csStorage.data.dark);
+      this.mode.addEventListener('click', () => {
+        const val = !(this.mode.dataset.dark == 'true');
+        csStorage.data.dark = val;
+        globalEvents.emit('theme-switch', val, this.mode, this.mode.children);
+        csStorage.refresh(csStorage.data);
+      });
+      if (!csStorage.data.dark) globalEvents.emit('theme-switch', csStorage.data.dark, this.mode, this.mode.children);
+      for (const btn of this.buttons) btn.addEventListener('click', () => tab.set(btn.dataset.name || 'home'));
+    };
+    this.spawn = function(name, copyableBtn) {
+      switch(name) {
+        case 'home': {
+          this.home.remove();
+          this.home = null;
+          this.home = copyableBtn.cloneNode(true);
+          this.home.dataset.name = 'home';
+          this.home.children[0].src = './assets/home.svg';
+          this.home.children[1].textContent = 'Back to Home';
+          this.home.style.boxShadow = 'inset 0 -5px 0 0 #0391a3';
+          this.node.insertBefore(this.home, copyableBtn);
+          this.home.addEventListener('click', () => {
+            tab.set('home');
+            globalEvents.emit('tab-switch', 'home');
+            this.home.remove();
+          });
+        };
+      }
+    };
+  })(this);
+  this.nav.attachListeners();
 
-  // other buttons
-  const navBtns = document.querySelectorAll(`div[class="nav-btn"]`);
-  for (let i = 1; i < navBtns.length; i++) {
-    const btn = navBtns[i];
-    btn.addEventListener("click", () => setTab(btn.id || "home"));
-  }
-}
-attachBtnListeners();
-
-/*
-  clone a nav button and replace it with a "home" button
-  copyableBtn -- nav element to clone
-*/
-function createHomeBtn(copyableBtn) {
-  const oldBtn = document.querySelector(`div[class="nav-btn"][id="home"]`);
-  if (oldBtn !== null) oldBtn.remove();
-  const homeBtn = copyableBtn.cloneNode(true);
-  const childs = homeBtn.children;
-  homeBtn.id = "home";
-  homeBtn.style.boxShadow = "inset 0 -5px 0 0 #0391a3";
-  childs[0].src = "/site-real/assets/home.svg";
-  childs[1].textContent = "Back to Home";
-
-  const nav = document.querySelector(`nav[class="nav-bar"]`)
-  nav.insertBefore(homeBtn, copyableBtn);
-  homeBtn.addEventListener("click", () => {
-    setTab("home");
-    EventEmitter.emit("tab-click", "home");
-    homeBtn.remove();
-  });
-}
-
-/*
-  create a new BR element (used in pages)
-*/
-const makeBreak = () => { return document.createElement("br") };
+  this.makeBreak = () => document.createElement("br");
+})();
