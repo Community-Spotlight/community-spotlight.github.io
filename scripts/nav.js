8c787573a35a9378cdf7d797e3dabf1173a7b447
みよ <135030944+yuri-kiss@users.noreply.github.com>
2024-09-13 19:26:25 -0400

    temporary data

---
+++ scripts/nav.js
@@ -0,0 +1,33 @@
+WindowEvents.on('GUI', (GUI) => {});
+
+```
+      <div class="logo" data-name="logo" role="button" aria-pressed="false" tabindex="1">
+          <img src="./assets/CS-logo.svg"></img>
+          <div style="padding: 0px 5px; margin-right: 5px;">Community Spotlight</div>
+      </div>
+      <div class="nav-btn" data-name="theme-switch" role="button" aria-pressed="false" tabindex="2">
+          <img src="./assets/light.svg"></img>
+          <span>Light Mode</span>
+      </div>
+      <div class="nav-btn" data-name="about" role="button" aria-pressed="false">
+        <img src="./assets/info.svg"></img>
+        <span>About Us</span>
+      </div>
+      <div class="nav-btn" data-name="submit" role="button" aria-pressed="false">
+        <img src="./assets/upload.svg"></img>
+        <span>Submit a Promo</span>
+      </div>
+      <div class="nav-btn" data-name="exports" role="button" aria-pressed="false">
+        <img src="./assets/eye.svg"></img>
+        <span>For Developers</span>
+      </div>
+      <div class="nav-btn" data-name="tos" role="button" aria-pressed="false">
+        <img src="./assets/paper.svg"></img>
+        <span>Terms of Service</span>
+      </div>
+      <div class="nav-btn" data-name="team" role="button" aria-pressed="false">
+        <img src="./assets/team.svg"></img>
+        <span>Our Team</span>
+      </div>
+```
+
