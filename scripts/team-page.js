15547b7357dadccad3831882e4f715e8dd5e7997
みよ <135030944+yuri-kiss@users.noreply.github.com>
2024-09-14 11:18:41 -0400

    Fix cache 3/3

--- scripts/team-page.js
+++ scripts/team-page.js
@@ -1,8 +1,9 @@
 GUI.tab.acquire(async (contentBody) => {
-  GUI_Imports.BasicCache.cache('users', 604800000, async () => {
-    const res = await fetch('../meta/users.json');
+  await GUI_Imports.BasicCache.cache('users', 604800000, async () => {
+    console.log('Caching users');
+    const res = await fetch('./meta/users.json');
     return await res.json();
   });
   const users = await GUI_Imports.BasicCache.get('users');
-  print(users);
+  console.log(users);
 });
