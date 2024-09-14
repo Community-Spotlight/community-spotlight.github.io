551bbcf778a7301d023a779c3e10d9e8ca5ad38e
みよ <135030944+yuri-kiss@users.noreply.github.com>
2024-09-13 20:33:43 -0400

    Fix tabindex

--- scripts/nav.js
+++ scripts/nav.js
@@ -11,11 +11,11 @@ window.GUI_Nav = new (function() {
     });
   };
   this.node = document.querySelector('[for="nav"]');
-  this.node.innerHTML = `<div class="logo" data-name="logo" role="button" aria-pressed="false" tabindex="0">
+  this.node.innerHTML = `<div class="logo" data-name="logo" role="button" aria-pressed="false" tabindex="0" data-tab="false">
     <img src="./assets/CS-logo.svg"></img>
     <div style="padding: 0px 5px; margin-right: 5px;">Community Spotlight</div>
   </div>
-  <div class="nav-btn" data-name="theme-switch" role="button" aria-pressed="false" tabindex="1">
+  <div class="nav-btn" data-name="theme-switch" role="button" aria-pressed="false" tabindex="1" data-tab="false">
     <img src="./assets/light.svg"></img>
     <span>Light Mode</span>
   </div>`;
@@ -26,7 +26,7 @@ window.GUI_Nav = new (function() {
     btn.dataset.name = String(name);
     btn.role = 'button';
     btn.setAttribute('aria-pressed', 'false');
-    btn.tabindex = String(this.tabindex);
+    btn.setAttribute('tabindex', String(this.tabindex));
     this.node.appendChild(btn);
     btn.innerHTML = `<img src="./assets/${asset}" /><span role="presentation"></span>`;
     btn.querySelector('span').textContent = text;
