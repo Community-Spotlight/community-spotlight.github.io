ccaea9ac0d237eb1b13ca4a0cf7610acd328e982
みよ <135030944+yuri-kiss@users.noreply.github.com>
2024-09-13 22:18:55 -0400

    No more home button

--- scripts/startup.js
+++ scripts/startup.js
@@ -41,22 +41,7 @@
     document.body.style.background = val === false ? '#ffffff' : '#141414';
     document.body.style.color = val === false ? '#000000' : '#ffffff';
   });
-  this.globalEvents.on('tab-switch', (name) => {
-    for (const btn of this.nav.buttons) {
-      if (btn.dataset.name !== name) {
-        btn.style.display = '';
-        btn.setAttribute('aria-hidden', 'false');
-        continue;
-      }
-      if (btn.dataset.name !== 'home') {
-        this.nav.spawn('home', btn);
-        btn.style.display = 'none';
-        btn.setAttribute('aria-hidden', 'true');
-        btn.setAttribute('aria-pressed', 'false');
-      }
-    }
-    this.tab.set(name);
-  });
+  this.globalEvents.on('tab-switch', (name) => this.tab.set(name));
   
   this.tab = new (function(GUI, GUI_Imports) {
     this.current = GUI_Imports.URLParams.get('page') || 'home';
@@ -108,7 +93,6 @@
     this.node = document.querySelector('nav.nav-bar');
     this.logo = document.querySelector('nav.nav-bar div.logo');
     this.theme = document.querySelector('div[data-name="theme-switch"]');
-    this.home = document.querySelector(`div.nav-btn[data-name="home"]`);
     function implementButtonEvents(btn, altcallback, tab, doBefore) {
       btn.altpress = altcallback || (() => {});
       btn.altdown = function(...args) {
@@ -141,11 +125,7 @@
       };
     };
     this.attachListeners = function() {
-      this.logo.onclick = () => {
-        if (this.home) this.home.remove();
-        this.home = null;
-        globalEvents.emit('tab-switch', 'home');
-      };
+      this.logo.onclick = () => globalEvents.emit('tab-switch', 'home');
       this.theme.dataset.dark = String(csStorage.data.dark);
       implementButtonEvents(this.theme, function(upEvent) {
         if (upEvent) return;
@@ -156,29 +136,6 @@
       }, false, false);
       if (!csStorage.data.dark) globalEvents.emit('theme-switch', csStorage.data.dark, this.theme, this.theme.children);
       for (const btn of this.buttons) implementButtonEvents(btn, () => {}, true, false);
-    };
-    this.spawn = function(name, copyableBtn) {
-      switch(name) {
-        case 'home': {
-          if (this.home) this.home.remove();
-          this.home = copyableBtn.cloneNode(true);
-          this.home.dataset.name = 'home';
-          this.home.children[0].src = './assets/home.svg';
-          this.home.children[1].textContent = 'Back to Home';
-          this.home.style.boxShadow = 'inset 0 -5px 0 0 #0391a3';
-          this.home.role = 'button';
-          this.home.setAttribute('aria-pressed', 'true');
-          this.home.setAttribute('tabindex', String(copyableBtn.getAttribute('tabindex')));
-          this.home.focus();
-          this.home.nav = this;
-          implementButtonEvents(this.home, function(upEvent) {
-            if (upEvent) return;
-            this.remove();
-            this.nav.home = null;
-          }, false, true);
-          this.node.insertBefore(this.home, copyableBtn);
-        };
-      }
     };
   })(this);
   this.globalEvents.emit('tab-switch', this.tab.current);
