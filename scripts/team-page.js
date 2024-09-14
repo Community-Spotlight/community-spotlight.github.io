f80e24fdf1de16bc154862a822448d3124b242b2
みよ <135030944+yuri-kiss@users.noreply.github.com>
2024-09-14 10:38:43 -0400

    Use the cache to fetch the users

--- scripts/team-page.js
+++ scripts/team-page.js
@@ -1,1 +1,8 @@
-
+GUI.tab.acquire(async (contentBody) => {
+  GUI_Imports.BasicCache.cache('users', 604800000, async () => {
+    const res = await fetch('../meta/users.json');
+    return await res.json();
+  });
+  const users = await GUI_Imports.BasicCache.get('users');
+  print(users);
+});
