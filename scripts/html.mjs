2a9d88a3916cac580a5096518fd0179af68549a5
yuri-kiss <135030944+yuri-kiss@users.noreply.github.com>
2024-12-23 09:55:53 -0500

    Simple preact init

---
+++ scripts/html.mjs
@@ -0,0 +1,31 @@
+import {
+  Component,
+  Fragment,
+  cloneElement,
+  createContext,
+  createElement,
+  createRef,
+  h,
+  hydrate,
+  isValidElement,
+  options,
+  render,
+  toChildArray,
+} from './preact.mjs';
+const Preact = {
+  Component,
+  Fragment,
+  cloneElement,
+  createContext,
+  createElement,
+  createRef,
+  h,
+  hydrate,
+  isValidElement,
+  options,
+  render,
+  toChildArray,
+};
+import htm from './htm.mjs';
+const html = htm.bind(h);
+export default { Preact, htm, html };
