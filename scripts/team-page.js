02739f230efab7649bd4d04a49abc49b4f3142f4
yuri-kiss <135030944+yuri-kiss@users.noreply.github.com>
2025-01-01 15:34:09 -0500

    Use GUI.imports instead of GUI_Imports

--- scripts/team-page.js
+++ scripts/team-page.js
@@ -1,9 +1,9 @@
 GUI.tab.acquire(async (contentBody) => {
-  await GUI_Imports.BasicCache.cache('users', 604800000, async () => {
+  await GUI.imports.BasicCache.cache('users', 604800000, async () => {
     console.log('Caching users');
     const res = await fetch('./meta/users.json');
     return await res.json();
   });
-  const users = await GUI_Imports.BasicCache.get('users');
+  const users = await GUI.imports.BasicCache.get('users');
   console.log(users);
 });
