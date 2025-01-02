1ce93ccc3cbf33791ebe4670a8d50c829c5ab69c
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-01-01 21:46:40 -0800

    test

--- scripts/team-page.js
+++ scripts/team-page.js
@@ -1,9 +1,15 @@
 GUI.tab.acquire(async (contentBody) => {
-  await GUI.imports.BasicCache.cache('users', 604800000, async () => {
-    console.log('Caching users');
-    const res = await fetch('./meta/users.json');
+  await GUI.imports.BasicCache.cache("users", 604800000, async () => {
+    console.log("Caching User List...");
+    const res = await fetch("./meta/users.json");
     return await res.json();
   });
-  const users = await GUI.imports.BasicCache.get('users');
+  const users = await GUI.imports.BasicCache.get("users");
+
   console.log(users);
-});
+  contentBody.appendChild(document.createTextNode('I am an example!'));
+}, `
+.content-body {
+  background-color: red;
+}
+`);
