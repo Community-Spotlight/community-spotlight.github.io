d5766bb5d358aec66f727c28abda2010f5f675f1
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-01-03 17:31:02 -0800

    Update startup.mjs

--- scripts/startup.mjs
+++ scripts/startup.mjs
@@ -49,6 +49,7 @@ globalThis.WindowEvents = new GUI_Imports.EventEmitter();
     this.data.dark ??= true;
   })(GUI_Imports);
   await (this.importScript('./scripts/nav.js').loadPromise);
+  await (this.importScript('https://raw.githubusercontent.com/Community-Spotlight/promotion-exports/refs/heads/main/scripts/community-spotlight.js').loadPromise);
   if (this.csStorage.data.dark) document.body.dataset.dark = 'true';
   else delete document.body.dataset.dark;
 
