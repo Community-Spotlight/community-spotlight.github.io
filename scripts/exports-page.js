4d8eec519656c66f7f09a89045c5915e6a18e2b7
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-01-08 00:58:36 -0800

    exports-page -- wip part 2

--- scripts/exports-page.js
+++ scripts/exports-page.js
@@ -1,3 +1,9 @@
+/*
+TODO
+-box for importing script
+-show ads
+-example return obj
+*/
 GUI.tab.acquire(async (contentBody) => {
   // load the Prism Library if not already loaded
   const PrismExists = document.querySelector(`script[class="Prism"]`);
@@ -19,23 +25,61 @@ GUI.tab.acquire(async (contentBody) => {
     download1: `First, download the most recent version of our Exports:`,
     download2: `Or, check out our <b>GitHub Repository:</b>`,
 
-    setup: `You have the freedom to promote any work of yours, including games, songs, websites, and more.`,
-    advert3: `To upload your Promotions, check out our Uploader Form by clicking the "Submit a Promo" Button above. You'll find our Guidelines and other rules there.`,
-    devs1: `Community Spotlight allows you to display Promotions for <b>free</b> with no required Sign-Ups or API keys!`,
-    devs2: `Since we are a <b>non-profit</b>, you will <b>not</b> be making any financial profit. You are simply helping other people promote their work.`,
-    devs3: `To display Promotions in your project, check out our Developer Exports by clicking the "For Developers" Button above.`,
-    contact1: `Connect with the Community Spotlight Team through our Discord!`,
-    contact2: `Join us to stay updated with announcements, get assistance, and engage with other promoters in the community! It's the perfect place to ask questions, share ideas, and collaborate.`,
+    setup1: `With our Exports, you have the choice to Fetch Promotions either <b>Online</b> or through <b>Stored Cache.</b>`,
+    setup2: `The difference between the two is that <b>Stored Cache</b> performs a <i>single</i> Fetch of the Promotion List and <b>saves it in the 'window' for future use,</b> while <b>Online Fetching</b> retrieves the list on <i>every</i> 'getOnlinePromoCS' call.`,
+    setup3: `Here’s how to setup the Exports:`,
+
+    usage1: `Here's how to use 'getOnlinePromoCS' and 'getCachedPromoCS' to retrieve specific Promotions based on specific Parameters. <i>(Note: all Parameters are <u>optional</u>)</i>`,
   };
   const code = {
     // excuse the formatting
     // it needs to be like this to not look bad in the site
     setup: `
-// comment test
-refreshPromos()
+/* Stored Cache */
+// use this to initialize the Cache
+// you may re-run this anytime to refresh the list
+await refreshPromoCacheCS();
+
+// you can now use this function for instant outputs
+// proper Parameters are described below
+getCachedPromoCS("image", {});
+
+
+/* Online */
+// no setup required, fetches the Promotion List
+// proper Parameters are described below
+await getOnlinePromoCS("video", {});
 `,
-    test: `
+    usage: `
+// both functions use the same Parameter rules
+// both will return JSON data for a radomized Promotion
+await getOnlinePromoCS(type, optParams);
+getCachedPromoCS(type, optParams);
+
+/*
+  Parameters:
+  - type: "image", "video", or "html"
+  - optParams:
+    tags: [array] – Valid tags can be found on our Uploader Site
+    aspectRatio (image): 250x250, 300x250, 480x270, 300x50, 50x300, 360x120, 120x360
+    aspectRatio (video): 1:1, 4:3, 4:5, 16:9, 9:16
+    videoLength (video): 5, 10, 15, 30
+    aspectRatio (html): 1:1, 4:3, 4:5, 16:9, 9:16
+*/
+
+// examples:
+getCachedPromoCS("image", {
+  tags: ["Gaming"],
+  aspectRatio: "300x250",
+});
+
+getCachedPromoCS("video", {
+  aspectRatio: "4:3",
+});
 `,
+    example: `
+
+`
   };
 
   const mainCard = document.createElement("div");
@@ -92,17 +136,27 @@ refreshPromos()
   cardBtns[1].addEventListener("click", (e) => handleClick(e, true));
   cardBtns[2].addEventListener("click", (e) => window.open("https://github.com/Community-Spotlight/promotion-exports", "_blank"));
 
-  const documentation = document.createElement("div");
-  documentation.classList.add("card-big");
-  documentation.innerHTML = `
+  const docsSetup = document.createElement("div");
+  docsSetup.classList.add("card-big");
+  docsSetup.innerHTML = `
     <div class="holder">
-      <div class="title">Documentation</div>
-      ${texts.setup.trim()}<br>
-      <pre><code class="language-javascript">${code.setup}</code></pre>
+      <div class="title">Documentation -- Setup</div>
+      ${texts.setup1}<br><br>${texts.setup2}<br><br>${texts.setup3}<br>
+      <pre><code class="language-javascript">${code.setup.trim()}</code></pre>
     </div>
   `;
 
-  contentBody.append(mainCard, downloadCard, documentation);
+  const docsUsage = document.createElement("div");
+  docsUsage.classList.add("card-big");
+  docsUsage.innerHTML = `
+    <div class="holder">
+      <div class="title">Documentation -- Fetching Promotion Data</div>
+      ${texts.usage1}<br>
+      <pre><code class="language-javascript">${code.usage.trim()}</code></pre>
+    </div>
+  `;
+
+  contentBody.append(mainCard, downloadCard, docsSetup, docsUsage);
 }, `
 .content-body {
   margin: 20px;
@@ -178,6 +232,12 @@ refreshPromos()
   margin: 15px;
 }
 .content-body code.language-javascript {
+  color: #e88922;
   text-shadow: none;
+  font-size: 0.82em;
+  line-height: 1;
+}
+.content-body code [class="token operator"] {
+  background: none;
 }
 `);
