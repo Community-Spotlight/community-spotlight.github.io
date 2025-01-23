ad203ac3fcaea7b1dc92cd9f334604494656a872
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-01-23 15:09:48 -0800

    tos-page -- wip part 2

--- scripts/tos-page.js
+++ scripts/tos-page.js
@@ -1,22 +1,56 @@
 GUI.tab.acquire((contentBody) => {
-  const titles = [
-    "test",
-    "test2",
-  ];
+  const email = "(need an email here, sharkpool!)";
+  const effectiveDate = "January 23, 2025";
+  const endMessage = `Thank you for reading and for using <b>Community Spotlight!</b> to share and support creative projects!`;
+
   const sections = {
-    test: `<b>Community Spotlight</b> is a <b>non-profit project</b> dedicated to supporting creators, artists, and developers by offering <b>free promotional services.</b>`,
-    test2: `<b>Community Spotlight</b> is a <b>non-profit project</b> dedicated to supporting creators, artists, and developers by offering <b>free promotional services.</b>`,
+    "Introduction":
+    `Welcome to <b>Community Spotlight</b>. These Terms of Service <b>("Terms")</b> govern your use of our advertising platform, which allows individuals to <b>advertise their projects for free</b> and developers to <b>display others' promotions for free</b>. By using our platform, you agree to these <b>Terms</b>.`,
+
+    "No Signup Required":
+    `<b>Community Spotlight</b> does not require <b>any signup or account creation</b> to display or embed promotions. <b>Community Spotlight</b> does not <b>collect, store, or process </b> any personal user data.`,
+
+    "Voluntary Participation":
+    `<b>All</b> contributions and interactions with <b>Community Spotlight</b> are entirely voluntary. By using <b>Community Spotlight</b>, you confirm that <b>you have the necessary rights</b> to promote the material. As well, Users <b>retain full ownership</b> of their uploaded promotions. By submitting a promotion, you agree that it does <b>not</b> violate any applicable laws or the rights of others.`,
+
+    "Prohibited Content":
+    `<b>Copyrighted content is prohibited</b> unless you have <b>explicit permission from the copyright owner</b>. Users must ensure their uploaded content <b>does not infringe on the intellectual property rights of others</b>. Content that is <b>unlawful, harmful, or otherwise inappropriate</b> is <b>strictly prohibited</b>. Content that is <b>generated or partially generated</b> with any LLM or Image Generation model (ChatGPT, Midjourney, Stable Diffusion, et cetera) <b>may not be promoted</b>. <b>Promotions generated with assistance or completely</b> from AI programs or tools will be handled on a <b>case-by-case</b> basis.`,
+
+    "Content Uploads":
+    `Users can upload <b>images, videos, and HTML embeds</b> to promote their projects. By uploading any promotional content, you grant <b>Community Spotlight</b> a <b>non-exclusive, royalty-free license</b> to display and share your content. You <b>retain ownership</b> of your content and are <b>solely responsible for its accuracy, legality, and compliance</b> with these <b>Terms</b>.`,
+
+    "Disclaimer of Liabilities":
+    `<b>Community Spotlight</b> is a free platform and <b>does not guarantee the performance, accuracy, or effectiveness</b> of any promotions displayed. We are <b>not responsible for any disputes or issues</b> arising from user promotions <b>unless it relates to IP or copyright</b>. As such, <b>promote and display promotions at your own risk.</b>`,
+
+    "Content Moderation":
+    `<b>Community Spotlight</b> reserves the right to <b>remove or refuse</b> any promotional material that <b>violates these Terms</b> or is deemed inappropriate. Promotions flagged by <b>users or moderators</b> may be subject to <b>review and removal without notice<b>.`,
+
+    "Intellectual Property":
+    `<b>Community Spotlight</b> respects <b>intellectual property rights</b> and expects users to <b>do the same</b>. If you believe your <b>copyright</b> has been <b>infringed upon</b>, please contact us at <b>${email}</b>.`,
+
+    "Changes to the Terms":
+    `<b>Community Spotlight</b> <b>reserves the right</b> to <b>modify these Terms</b> at any time. Updates will be posted on this page with an updated effective date. Continued use of the platform <b>constitutes acceptance</b> of the revised <b>Terms.</b>`,
+
+    "Service Availablitiy":
+    `<b>Community Spotlight</b> is provided <b>as is</b> and <b>as available</b>. We make no <b>guarantees regarding uptime, availability, or functionality</b>. We reserve the right to <b>suspend or discontinue</b> any part of the service <b>without prior notice</b>.`,
+
+    "Governing Law":
+    `These <b>Terms</b> are <b>governed by</b> and <b>constructed in accordance with</b> the laws of <b>Canada</b>. Any <b>disputes</b> arising under these <b>Terms shall be resolved in</b> the courts of <b>Canada</b>.`,
+
+    "Contact Us":
+    `If you have any <b>questions or concerns</b> regarding these <b>Terms</b> or about <b>Community Spotlight</b>, please contact us at <b>${email}</b>.`,
+
   };
 
-  const makeCapsule = (title) => {
+  const makeCapsule = (entry) => {
     const capsule = document.createElement("div");
     capsule.classList.add("capsule");
     capsule.innerHTML = `
       <div class="capsule-contain">
         <img class="flipper-img" draggable="false" src="https://raw.githubusercontent.com/Community-Spotlight/assets/refs/heads/main/arrow-right.svg">
-        <div class="header">${title}</div>
+        <div class="header">${entry[0]}</div>
       </div>
-      <div class="text">${sections[title]}</div>
+      <div class="text">${entry[1]}</div>
     `;
 
     const arrow = capsule.querySelector(`img[class="flipper-img"]`);
@@ -45,12 +79,23 @@ GUI.tab.acquire((contentBody) => {
     <img class="logo-img" draggable="false" src="https://raw.githubusercontent.com/Community-Spotlight/assets/refs/heads/main/CS-logo.svg">
     <div class="holder">
       <div class="title">Terms of Service</div>
+      <b>Effective: ${effectiveDate}</b>
+    </div>
+  `;
+
+  const endCard = document.createElement("div");
+  endCard.classList.add("card");
+  endCard.innerHTML = `
+    <div class="holder">
+      <div class="title">The End</div>
+      ${endMessage}
     </div>
   `;
 
   const holder = mainCard.querySelector(`div[class="holder"]`);
-  for (let i = 0; i < titles.length; i++) holder.appendChild(makeCapsule(titles[i]));
-  contentBody.append(mainCard);
+  const entries = Object.entries(sections);
+  for (let i = 0; i < entries.length; i++) holder.appendChild(makeCapsule(entries[i]));
+  contentBody.append(mainCard, endCard);
 }, `
 .content-body {
   margin: 20px;
